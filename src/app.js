2c5105b8482435f5d02f391b69c91cd0f9a017de
basmaklida-web <basmaklida@gmail.com>
2026-02-13 20:39:21 +0100

    Add expense routes and home route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+// 1. Zidi l-import dyal expenseRoutes hna
+const expenseRoutes = require('./routes/expenseRoutes'); 
 
 const app = express();
 
@@ -9,5 +11,12 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+// 2. Zidi had l-starr bach l-Frontend i-qder i-sift l-masarif
+app.use('/api/expenses', expenseRoutes); 
+
+// 3. (Ikhtiyari) Zidi had l-test bach t-choufiha f l-browser dghya
+app.get('/', (req, res) => {
+    res.send("Backend is running successfully!");
+});
 
 module.exports = app;
